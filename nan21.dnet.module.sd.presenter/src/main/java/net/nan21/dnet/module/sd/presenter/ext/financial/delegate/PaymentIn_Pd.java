72acf897f081ff7c471959d1346722635cfe8ad1
Attila Mathe <attila.mathe@dnet-ebusiness-suite.com>
2013-11-05 13:35:15 +0200

    Payment-in delegate

---
+++ nan21.dnet.module.sd.presenter/src/main/java/net/nan21/dnet/module/sd/presenter/ext/financial/delegate/PaymentIn_Pd.java
@@ -0,0 +1,65 @@
+package net.nan21.dnet.module.sd.presenter.ext.financial.delegate;
+
+import net.nan21.dnet.core.presenter.service.AbstractPresenterDelegate;
+import net.nan21.dnet.module.sd.presenter.impl.financial.model.PaymentIn_Ds;
+import net.nan21.dnet.module.tx.business.api.financial.IPaymentService;
+import net.nan21.dnet.module.tx.domain.impl.financial.Payment;
+
+public class PaymentIn_Pd extends AbstractPresenterDelegate {
+
+	/**
+	 * Confirm document.<BR>
+	 * Delegate to the proper business service.
+	 * 
+	 * @param ds
+	 * @throws Exception
+	 */
+	public void confirm(PaymentIn_Ds ds) throws Exception {
+		IPaymentService srv = ((IPaymentService) this
+				.findEntityService(Payment.class));
+		Payment e = srv.findById(ds.getId());
+		srv.doConfirm(e);
+	}
+
+	/**
+	 * Un-Confirm document.<BR>
+	 * Delegate to the proper business service.
+	 * 
+	 * @param ds
+	 * @throws Exception
+	 */
+	public void unConfirm(PaymentIn_Ds ds) throws Exception {
+		IPaymentService srv = ((IPaymentService) this
+				.findEntityService(Payment.class));
+		Payment e = srv.findById(ds.getId());
+		srv.doUnConfirm(e);
+	}
+
+	/**
+	 * Post document to accounting.<BR>
+	 * Delegate to the proper business service.
+	 * 
+	 * @param ds
+	 * @throws Exception
+	 */
+	public void post(PaymentIn_Ds ds) throws Exception {
+		IPaymentService srv = ((IPaymentService) this
+				.findEntityService(Payment.class));
+		Payment e = srv.findById(ds.getId());
+		srv.doPost(e);
+	}
+
+	/**
+	 * Un-Post document from accounting.<BR>
+	 * Delegate to the proper business service.
+	 * 
+	 * @param ds
+	 * @throws Exception
+	 */
+	public void unPost(PaymentIn_Ds ds) throws Exception {
+		IPaymentService srv = ((IPaymentService) this
+				.findEntityService(Payment.class));
+		Payment e = srv.findById(ds.getId());
+		srv.doUnPost(e);
+	}
+}
